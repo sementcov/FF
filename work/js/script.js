26f8f4e3c20bf1eaba47f351d4152611be98f799
sementcov <86025023+sementcov@users.noreply.github.com>
2021-06-19 05:56:01 +0300

    Add files via upload

--- work/js/script.js
+++ work/js/script.js
@@ -1,14 +1,14 @@
-var countDownDate = new Date("May 17, 2022 03:00:00").getTime();
-var countDownFunction = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 *60 *24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("timer").innerHTML = days + "д " + hours + "ч " + minutes + "м " + seconds + "с "; 
+let countDownDate = new Date("May 17, 2024 03:00:00").getTime();
+let countDownFunction = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 *60 *24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "; 
     if (distance < 0) {
         clearInterval(countDownFunction);
-        document.getElementById("timer").innerHTML = "Проект запущен";
+        document.querySelector("timer").innerHTML = "Проект запущен";
     }
 }, 1000)
